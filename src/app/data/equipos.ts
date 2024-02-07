7661eef0417065ca0d87579d6a6776ed05519616
Sofia <sofia.jimlap@educa.jcyl.es>
2024-02-07 13:01:05 +0100

    .

---
+++ src/app/data/equipos.ts
@@ -0,0 +1,85 @@
+import {Equipo} from "../modelos/equipo";
+
+export const equipos_data : Equipo[] = [
+  {
+    "nombre": "Athletic Club",
+    "id": "ATHCLU(ESP)"
+
+  },
+  {
+    "nombre": "Getafe",
+    "id": "GETAFE(ESP)"
+  },
+  {
+    "nombre": "Barcelona",
+    "id": "FCBARC(ESP)"
+  },
+  {
+    "nombre": "Betis",
+    "id": "BETIS(ESP)"
+  },
+  {
+    "nombre": "Deportivo",
+    "id": "DEPORT(ESP)"
+  },
+  {
+    "nombre": "Real Madrid",
+    "id": "RMADRI(ESP)"
+  },
+  {
+    "nombre": "Levante",
+    "id": "LEVANT(ESP)"
+  },
+  {
+    "nombre": "Villareal",
+    "id": "VIREAL(ESP)"
+  },
+  {
+    "nombre": "Málaga",
+    "id": "MALAGA(ESP)"
+  },
+  {
+    "nombre": "Eibar",
+    "id": "EIBAR(ESP)"
+  },
+  {
+    "nombre": "Leganés",
+    "id": "LEGANE(ESP)"
+  },
+  {
+    "nombre": "Alavés",
+    "id": "ALAVES(ESP)"
+  },
+  {
+    "nombre": "Valencia",
+    "id": "VALENC(ESP)"
+  },
+  {
+    "nombre": "Las Palmas",
+    "id": "LASPAL(ESP)"
+  },
+  {
+    "nombre": "Celta",
+    "id": "CELTAV(ESP)"
+  },
+  {
+    "nombre": "Real Sociedad",
+    "id": "RSOCIE(ESP)"
+  },
+  {
+    "nombre": "Girona",
+    "id": "GIRONA(ESP)"
+  },
+  {
+    "nombre": "Atlético de Madrid",
+    "id": "ATLMAD(ESP)"
+  },
+  {
+    "nombre": "Sevilla",
+    "id": "SEVILA(ESP)"
+  },
+  {
+    "nombre": "Español",
+    "id": "ESPANO(ESP)"
+  }
+]
--- src/app/data/partidos.data.ts
+++ src/app/data/partidos.data.ts
@@ -1,24 +1,148 @@
 import {Partido} from "../modelos/partido";
 
-export const partidos_data: Partido[] = [{
-  "idLocal" : "ATHCLU(ESP)",
-  "idVisitante" : "DEPORT(ESP)",
-  "fechaHora" : "15/2/2020 8:49",
-  "goles" : [
-    {
-      "idParticipante" : "AUTHCLU(ESP)",
-      "fechaHora" : "15/2/2020 9:20"
-    },
-    {
-      "idParticipante" : "AUTHCLU(ESP)",
-      "fechaHora" : "15/2/2020 9:35"
-    },
-  ],
-  "tarjetas" : [
-    {
-      "idParticipante" : "DEPORT(ESP)",
-      "tipoTarjeta" : "AMARILLA",
-      "fechaHora" : "15/2/2020 9:50"
-    }
-  ]
-}]
+export const partidos_data: Partido[] = [
+  {
+    "idLocal" : "ATHCLU(ESP)",
+    "idVisitante" : "DEPORT(ESP)",
+    "fechaHora" : "15/2/2020 8:49",
+    "goles" : [
+      {
+        "idParticipante" : "AUTHCLU(ESP)",
+        "fechaHora" : "15/2/2020 9:20"
+      },
+      {
+        "idParticipante" : "AUTHCLU(ESP)",
+        "fechaHora" : "15/2/2020 9:35"
+      },
+    ],
+    "tarjetas" : [
+      {
+        "idParticipante" : "DEPORT(ESP)",
+        "tipoTarjeta" : "AMARILLA",
+        "fechaHora" : "15/2/2020 9:50"
+      }
+    ]
+  }, {
+    "idLocal": "FCBARC(ESP)",
+    "idVisitante": "VIREAL(ESP)",
+    "fechaHora": "15/2/2020 9:35",
+    "goles": [
+      {
+        "idParticipante": "FCBARC(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "VIREAL(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "VIREAL(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ],
+    "tarjetas": [
+      {
+        "idParticipante": "FCBARC(ESP)",
+        "tipoTarjeta": "ROJA",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ]
+  }, {
+    "idLocal": "MALAGA(ESP)",
+    "idVisitante": "EIBAR(ESP)",
+    "fechaHora": "15/2/2020 9:35",
+    "goles": [
+      {
+        "idParticipante": "EIBAR(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "MALAGA(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "MALAGA(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "MALAGA(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ],
+    "tarjetas": [
+      {
+        "idParticipante": "MALAGA(ESP)",
+        "tipoTarjeta": "ROJA",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "EIBAR(ESP)",
+        "tipoTarjeta": "AMARILLA",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "EIBAR(ESP)",
+        "tipoTarjeta": "AMARILLA",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ]
+  },
+  {
+    "idLocal": "GETAFE(ESP)",
+    "idVisitante": "BETIS(ESP)",
+    "fechaHora": "15/2/2020 9:35",
+    "goles": [
+      {
+        "idParticipante": "BETIS(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ],
+    "tarjetas": [
+      {
+        "idParticipante": "GETAFE(ESP)",
+        "tipoTarjeta": "ROJA",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ]
+  },
+  {
+    "idLocal": "RMADRID(ESP)",
+    "idVisitante": "LEVANT(ESP)",
+    "fechaHora": "15/2/2020 9:35",
+    "goles": [
+      {
+        "idParticipante": "LEVANT(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "LEVANT(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "LEVANT(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "LEVANT(ESP)",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ],
+    "tarjetas": [
+      {
+        "idParticipante": "RMADRID(ESP)",
+        "tipoTarjeta": "ROJA",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "RMADRID(ESP)",
+        "tipoTarjeta": "ROJA",
+        "fechaHora": "15/2/2020 9:35",
+      },
+      {
+        "idParticipante": "LEVANT(ESP)",
+        "tipoTarjeta": "AMARILLA",
+        "fechaHora": "15/2/2020 9:35",
+      }
+    ]
+  }
+]
