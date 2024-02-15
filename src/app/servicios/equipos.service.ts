ce74eed02e5378103967e53d4cf505155c08b6d4
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-02-15 16:55:25 +0100

    service

--- src/app/servicios/equipos.service.ts
+++ src/app/servicios/equipos.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Equipo} from "../modelos/equipo";
 import {equipos_data} from "../data/equipos";
+import {PartidosService} from "./partidos.service";
+import {Gol, Partido, Tarjeta} from "../modelos/partido";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EquiposService {
 
-  constructor() { }
+  partidos : Partido[] = []
+  constructor(private partidosService : PartidosService) { }
 
   getEquipos () : Observable<Equipo[]>{
         return of (equipos_data)
@@ -22,5 +25,31 @@ export class EquiposService {
       }
     }
     return e!
+  }
+
+  buscarGoles (id : string){
+    let goles : Gol[] = []
+    let partidos : Partido[] = this.partidosService.partidosEquipo(id)
+    for (let partido of partidos){
+      for(let gol of partido.goles){
+        if(gol.idParticipante == id){
+          goles.push(gol)
+        }
+      }
+    }
+    return goles
+  }
+
+  buscarTarjetas (id: string){
+    let tarjetas : Tarjeta[] = []
+    let partidos : Partido[] = this.partidosService.partidosEquipo(id)
+    for (let partido of partidos){
+      for(let tarjeta of partido.tarjetas){
+        if(tarjeta.idParticipante == id){
+          tarjetas.push(tarjeta)
+        }
+      }
+    }
+    return tarjetas
   }
 }
