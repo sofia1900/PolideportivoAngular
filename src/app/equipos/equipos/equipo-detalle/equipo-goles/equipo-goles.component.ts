abf7650724956628485500648235ac04e0427979
Sofia <sofia.jimlap@educa.jcyl.es>
2024-02-19 12:51:26 +0100

    .

--- src/app/equipos/equipos/equipo-detalle/equipo-goles/equipo-goles.component.ts
+++ src/app/equipos/equipos/equipo-detalle/equipo-goles/equipo-goles.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Gol} from "../../../../modelos/partido";
+import {ActivatedRoute} from "@angular/router";
+import {EquiposService} from "../../../../servicios/equipos.service";
 
 @Component({
   selector: 'app-equipo-goles',
   templateUrl: './equipo-goles.component.html',
   styleUrl: './equipo-goles.component.css'
 })
-export class EquipoGolesComponent {
+export class EquipoGolesComponent implements OnInit {
+  idEquipo! : string;
+  goles : Gol[] = []
 
+  constructor(private route : ActivatedRoute, private equipoService : EquiposService) {}
+
+  ngOnInit(){
+    this.idEquipo = this.route.snapshot.params['id'];
+    this.goles = this.equipoService.buscarGoles(this.idEquipo)
+  }
 }
--- src/app/equipos/equipos/equipo-detalle/equipo-tarjetas/equipo-tarjetas.component.ts
+++ src/app/equipos/equipos/equipo-detalle/equipo-tarjetas/equipo-tarjetas.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Tarjeta} from "../../../../modelos/partido";
+import {ActivatedRoute} from "@angular/router";
+import {EquiposService} from "../../../../servicios/equipos.service";
 
 @Component({
   selector: 'app-equipo-tarjetas',
   templateUrl: './equipo-tarjetas.component.html',
   styleUrl: './equipo-tarjetas.component.css'
 })
-export class EquipoTarjetasComponent {
+export class EquipoTarjetasComponent implements OnInit{
+  idEquipo! : string;
+  tarjetas : Tarjeta[] = []
 
+  constructor(private route : ActivatedRoute, private equipoService : EquiposService) {}
+
+  ngOnInit(){
+    this.idEquipo = this.route.snapshot.params['id'];
+    this.tarjetas = this.equipoService.buscarTarjetas(this.idEquipo)
+  }
 }
